d19664af10a111022d25af74da49745da60fd6bb
javiortig <javiortig@localhost.localdomain>
2020-12-03 16:23:46 +0100

    f7

---
+++ Assets/pruebecillas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruebecillas : MonoBehaviour
+{   
+    public CharacterController characterController;
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        characterController.Move(
+            new Vector3(-2*Time.deltaTime, 0f, 0f));
+    }
+
+}
